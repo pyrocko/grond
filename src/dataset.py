pyrocko/grond

commit f2c621771f10cc59bfb54c9f4bdd694ef84df2eb

avoid an unneccessary import of pyrocko.snuffling

src/dataset.py
@@ -3,7 +3,7 @@ import copy
 import logging
 from collections import defaultdict
 import numpy as num
-from pyrocko import util, pile, model, config, trace, snuffling, \
+from pyrocko import util, pile, model, config, trace, \
     marker as pmarker
 from pyrocko.fdsn import enhanced_sacpz, station as fs
 from pyrocko.guts import Object, Tuple, String, Float, dump_all, load_all

commit 61e4a46e89134e509bcf21a9fbcf614e8bec0833

fix error introduced in f2c621771f10cc59bfb54c9f4bdd694ef84df2eb

src/dataset.py
@@ -145,7 +145,7 @@ class Dataset(object):
                     fs.load_xml(filename=stationxml_filename))
 
     def add_clippings(self, markers_filename):
-        markers = snuffling.load_markers(markers_filename)
+        markers = pmarker.load_markers(markers_filename)
         clippings = {}
         for marker in markers:
             nslc = marker.one_nslc()