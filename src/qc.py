0f9e71fc6074bcebfe6efa3189fdc6c012fd534d
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2017-06-08 17:11:15 +0200

    new subcommand: qc-polarization

--- src/qc.py
+++ src/qc.py
@@ -6,7 +6,7 @@ import numpy as num
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 
-from pyrocko import gf, trace, orthodrome as od, plot
+from pyrocko import gf, orthodrome as od, plot
 from grond import dataset
 
 km = 1000.
@@ -14,7 +14,37 @@ km = 1000.
 logger = logging.getLogger('grond.qc')
 
 
-def polarization(ds, store, timing, fmin, fmax, ffactor=1.5, tfactor=2.):
+def darken(c):
+    return (c[0]*0.5, c[1]*0.5, c[2]*0.5)
+
+
+def plot_color_line(axes, x, y, t, color, tmin, tmax):
+    from matplotlib.colors import LinearSegmentedColormap
+    from matplotlib.collections import LineCollection
+
+    cmap = LinearSegmentedColormap.from_list(
+        'noname', [color, (1., 1., 1.)], 100)
+
+    points = num.array([x, y], dtype=num.float).T.reshape(-1, 1, 2)
+    segments = num.concatenate([points[:-1], points[1:]], axis=1)
+    lc = LineCollection(segments, cmap=cmap, norm=plt.Normalize(tmin, tmax))
+    lc.set_array(t)
+    axes.add_collection(lc, autolim=True)
+
+
+def polarization(
+        ds, store, timing, fmin, fmax, ffactor,
+        time_factor=2.,
+        distance_min=None,
+        distance_max=None,
+        depth_min=None,
+        depth_max=None,
+        size_factor=0.05,
+        nsl_to_time=None,
+        output_filename=None,
+        output_format=None,
+        output_dpi=None):
+
     event = ds.get_event()
     stations = ds.get_stations()
 
@@ -26,14 +56,37 @@ def polarization(ds, store, timing, fmin, fmax, ffactor=1.5, tfactor=2.):
     trs = []
     for station in stations:
 
+        nsl = station.nsl()
+
+        dist = source.distance_to(station)
+
+        if distance_min is not None and dist < distance_min:
+            continue
+
+        if distance_max is not None and distance_max < dist:
+            continue
+
+        if depth_min is not None and station.depth < depth_min:
+            continue
+
+        if depth_max is not None and depth_max < station.depth:
+            continue
+
+        if nsl_to_time is None:
+            tp = event.time + store.t(timing, source, station)
+
+        else:
+            if nsl not in nsl_to_time:
+                continue
+
+            tp = nsl_to_time[nsl]
+
         for component in 'ZNE':
 
-            ttp = store.t(timing, source, station)
+            tmin = tp - time_factor / fmin
+            tmax = tp + time_factor / fmin
 
-            tmin = event.time + ttp - tfactor / fmin
-            tmax = event.time + ttp + tfactor / fmin
-
-            nslc = station.nsl() + (component,)
+            nslc = nsl + (component,)
 
             freqlimits = [
                 fmin / ffactor,
@@ -42,9 +95,6 @@ def polarization(ds, store, timing, fmin, fmax, ffactor=1.5, tfactor=2.):
                 fmax * ffactor]
 
             tfade = 1.0 / (fmin / ffactor)
-
-            tlong = 5.0 / fmin
-            tshort = 0.2 * tlong
 
             try:
                 trs_projected, trs_restituted, trs_raw = \
@@ -56,9 +106,6 @@ def polarization(ds, store, timing, fmin, fmax, ffactor=1.5, tfactor=2.):
                         freqlimits=freqlimits,
                         debug=True)
 
-                # for tr in trs_projected:
-                #     tr.sta_lta_right(tshort, tlong)
-
                 trs.extend(trs_projected)
 
             except dataset.NotFound, e:
@@ -69,9 +116,9 @@ def polarization(ds, store, timing, fmin, fmax, ffactor=1.5, tfactor=2.):
     for tr in trs:
         d_trs[tr.nslc_id[:3]][tr.nslc_id[3]] = tr
 
-    fontsize = 11.
+    fontsize = 10.
     plot.mpl_init(fontsize=fontsize)
-    fig = plt.figure(figsize=(8., 8.))
+    fig = plt.figure(figsize=plot.mpl_papersize('a4', 'landscape'))
     plot.mpl_margins(fig, w=7., h=6., units=fontsize)
 
     grid = ImageGrid(
@@ -80,8 +127,6 @@ def polarization(ds, store, timing, fmin, fmax, ffactor=1.5, tfactor=2.):
         add_all=True,
         label_mode='L',
         aspect=True)
-
-    print dir(grid)
 
     axes_en = grid[0]
     axes_en.set_ylabel('Northing [km]')
@@ -101,13 +146,37 @@ def polarization(ds, store, timing, fmin, fmax, ffactor=1.5, tfactor=2.):
 
     grid[3].set_axis_off()
 
-    factor = 5000.
-
+    locations = []
     for nsl in sorted(d_trs.keys()):
+        station = nsl_to_station[nsl]
+        n, e = od.latlon_to_ne(
+            event.lat, event.lon, station.lat, station.lon)
 
-        tr_e = d_trs[nsl].get('E', None)
-        tr_n = d_trs[nsl].get('N', None)
-        tr_z = d_trs[nsl].get('Z', None)
+        locations.append((n, e))
+
+    ns, es = num.array(locations, dtype=num.float).T
+
+    n_min = num.min(ns)
+    n_max = num.max(ns)
+    e_min = num.min(es)
+    e_max = num.max(es)
+
+    factor = max((n_max - n_min) * size_factor, (e_max - e_min) * size_factor)
+
+    fontsize_annot = fontsize * 0.7
+
+    data = {}
+    for insl, nsl in enumerate(sorted(d_trs.keys())):
+
+        color = plot.mpl_graph_color(insl)
+
+        try:
+            tr_e = d_trs[nsl]['E']
+            tr_n = d_trs[nsl]['N']
+            tr_z = d_trs[nsl]['Z']
+
+        except KeyError:
+            continue
 
         station = nsl_to_station[nsl]
 
@@ -116,26 +185,63 @@ def polarization(ds, store, timing, fmin, fmax, ffactor=1.5, tfactor=2.):
 
         d = station.depth
 
-        axes_en.plot(e/km, n/km, '^')
-        axes_dn.plot(d/km, n/km, '^')
-        axes_ed.plot(e/km, d/km, '^')
+        axes_en.annotate(
+            '.'.join(x for x in nsl if x),
+            xy=(e/km, n/km),
+            xycoords='data',
+            xytext=(fontsize_annot/3., fontsize_annot/3.),
+            textcoords='offset points',
+            verticalalignment='bottom',
+            horizontalalignment='left',
+            rotation=0.,
+            size=fontsize_annot)
 
-        arr_e = tr_e.ydata if tr_e else None
-        arr_n = tr_n.ydata if tr_n else None
-        arr_z = tr_z.ydata if tr_z else None
+        axes_en.plot(e/km, n/km, '^', mfc=color, mec=darken(color))
+        axes_dn.plot(d/km, n/km, '^', mfc=color, mec=darken(color))
+        axes_ed.plot(e/km, d/km, '^', mfc=color, mec=darken(color))
 
-        amax = num.max(num.abs(num.sqrt(arr_e**2 + arr_n**2 + arr_z**2)))
-        axes_en.plot(
-            (e + arr_e/amax * factor)/km, (n + arr_n/amax * factor)/km,
-            color=plot.mpl_color('skyblue2'))
-        axes_dn.plot(
+        arr_e = tr_e.ydata
+        arr_n = tr_n.ydata
+        arr_z = tr_z.ydata
+        arr_t = tr_z.get_xdata()
+
+        data[nsl] = (arr_e, arr_n, arr_z, arr_t, n, e, d, color)
+
+    amaxs = []
+    amax_hors = []
+    for nsl in sorted(data.keys()):
+        arr_e, arr_n, arr_z, arr_t, n, e, d, color = data[nsl]
+        amaxs.append(
+            num.max(num.abs(num.sqrt(arr_e**2 + arr_n**2 + arr_z**2))))
+        amax_hors.append(
+            num.max(num.abs(num.sqrt(arr_e**2 + arr_n**2))))
+
+    amax = num.median(amaxs)
+    amax_hor = num.median(amax_hors)
+
+    for nsl in sorted(data.keys()):
+        arr_e, arr_n, arr_z, arr_t, n, e, d, color = data[nsl]
+        tmin = arr_t.min()
+        tmax = arr_t.max()
+        plot_color_line(
+            axes_en,
+            (e + arr_e/amax_hor * factor)/km, (n + arr_n/amax_hor * factor)/km,
+            arr_t, color, tmin, tmax)
+        plot_color_line(
+            axes_dn,
             (d - arr_z/amax * factor)/km, (n + arr_n/amax * factor)/km,
-            color=plot.mpl_color('skyblue2'))
-        axes_ed.plot(
+            arr_t, color, tmin, tmax)
+        plot_color_line(
+            axes_ed,
             (e + arr_e/amax * factor)/km, (d - arr_z/amax * factor)/km,
-            color=plot.mpl_color('skyblue2'))
+            arr_t, color, tmin, tmax)
 
     axes_ed.invert_yaxis()
 
-    plt.show()
-    # trace.snuffle(trs)
+    for axes in (axes_dn, axes_ed, axes_en):
+        axes.autoscale_view(tight=True)
+
+    if output_filename is None:
+        plt.show()
+    else:
+        fig.savefig(output_filename, format=output_format, dpi=output_dpi)
