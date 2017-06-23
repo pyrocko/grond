0abeaa4985bae64b2ad4f1130b6e7eb1eab1e9d4
miili <isken@gfz-potsdam.de>
2017-06-23 14:24:05 +0200

    Added version string

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,8 @@ from .dataset import *  # noqa
 from .problems import *  # noqa
 from .targets import *  # noqa
 from .meta import *  # noqa
-from .synthetic_tests import *  #noqa
+from .synthetic_tests import *  # noqa
 from .solvers import *  # noqa
+
+
+__version__ = '0.2'
