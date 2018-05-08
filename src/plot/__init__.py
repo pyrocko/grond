da560a999783ea6e38aec66c20f31bcec7786977
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2018-05-08 12:44:55 +0200

    plotting: add missing import

--- src/plot/__init__.py
+++ src/plot/__init__.py
@@ -1,5 +1,6 @@
 from grond.problems import plot  # noqa
 from grond.targets import plot  # noqa
+from grond.optimisers import plot  # noqa
 
 from grond.plot.config import *  # noqa
 from grond.plot.main import *  # noqa
