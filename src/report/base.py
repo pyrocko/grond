e765c0ecb11c1c8d60421b8daf7e19c408606ebf
miili <isken@gfz-potsdam.de>
2018-05-28 13:57:26 +0200

    report: changed to 4.4.4.4

--- src/report/base.py
+++ src/report/base.py
@@ -204,7 +204,7 @@ def serve_ip(host):
     elif host == 'default':
         import socket
         ip = [
-            (s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close())
+            (s.connect(('4.4.4.4', 80)), s.getsockname()[0], s.close())
             for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
     elif host == '*':
         ip = ''
