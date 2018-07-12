138be4b370e00102586ce6eeedebbbb0680839de
asteinbe <asteinb@gfz-potsdam.de>
2018-07-12 19:50:55 +0200

    tr var 0 to nan

--- src/analysers/noise_analyser/analyser.py
+++ src/analysers/noise_analyser/analyser.py
@@ -83,8 +83,8 @@ def seismic_noise_variance(traces, engine, event, targets,
         stat_w = 1.
 
         if tr is None:
-            var_ds.append(0.)
-            ev_ws.append(0.)
+            var_ds.append(num.nan)
+            ev_ws.append(num.nan)
         else:
 
             arrival_time = get_phase_arrival_time(
@@ -215,7 +215,8 @@ class NoiseAnalyser(Analyser):
                 logger.info(
                     'Noise Analyser returned a weight of 0 for all stations')
 
-        weights = norm_noise/var_ds
+        weights0 = norm_noise/var_ds
+        weights = num.where(var_ds==num.nan, 0., weights0)
         if self.check_events:
             weights = weights*ev_ws
 
