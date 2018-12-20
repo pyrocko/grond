034745dbd6ecfc62eb900607b7b717f7973eac1d
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2018-12-20 10:48:28 +0100

    setup.py: support older setuptools versions

--- setup.py
+++ setup.py
@@ -137,8 +137,9 @@ setup(
 
             'data/snippets/*.gronf',
             'data/snippets/*.md',
-            'data/examples/*/*',
-            'data/examples/*/*/*'
+            'data/examples/*/*.*',
+            'data/examples/*/*/*.*'
+            'data/examples/*/*/grondown'
             ]},
 
     data_files=[],
