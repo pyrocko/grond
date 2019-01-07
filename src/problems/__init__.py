f8e4c3d752339b10be57a489e4cc971ecd9c46d1
miili <isken@gfz-potsdam.de>
2019-01-07 18:34:54 +0100

    Added new VolumeProblem

--- src/problems/__init__.py
+++ src/problems/__init__.py
@@ -1,8 +1,8 @@
 '''
 Definition of the objective function and source model parameter space.
 '''
-
 from .base import *  # noqa
 from .cmt.problem import *  # noqa
 from .rectangular.problem import *  # noqa
 from .double_dc.problem import *  # noqa
+from .volume.problem import *  # noqa
