da697ff5bfe12131c5bd66717e8a205043b3ae91
braunfuss <asteinbe@gfz-potsdam.de>
2019-04-29 20:51:53 +0200

    init update

--- src/problems/__init__.py
+++ src/problems/__init__.py
@@ -4,5 +4,6 @@ Definition of the objective function and source model parameter space.
 from .base import *  # noqa
 from .cmt.problem import *  # noqa
 from .rectangular.problem import *  # noqa
+from .multirectangular.problem import *  # noqa
 from .double_dc.problem import *  # noqa
 from .volume_point.problem import *  # noqa
