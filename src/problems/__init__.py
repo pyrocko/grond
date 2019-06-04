4528c0bd1154a55999eb2b3c57da42c7fb81a2e7
miili <isken@gfz-potsdam.de>
2019-06-04 10:35:49 +0200

    problems: pass on ImportError

--- src/problems/__init__.py
+++ src/problems/__init__.py
@@ -1,9 +1,17 @@
 '''
 Definition of the objective function and source model parameter space.
 '''
+import warnings
+
 from .base import *  # noqa
 from .cmt.problem import *  # noqa
 from .rectangular.problem import *  # noqa
 from .double_dc.problem import *  # noqa
 from .volume_point.problem import *  # noqa
-from .vlvd.problem import *  # noqa
+
+try:
+    from .vlvd.problem import *  # noqa
+except ImportError:
+    warnings.warn(
+        'could not import pyrocko.gf.VLVDSource. Update pyrocko to enable'
+        ' inversion of VLVDProblem.', ImportWarning)
