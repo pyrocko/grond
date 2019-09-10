93d8cedd7c2e00f353541205699e6e8e3f2edf89
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2019-09-10 12:26:18 +0200

    testing: quiet future warings and decrease log level to warnings

--- test/__init__.py
+++ test/__init__.py
@@ -3,3 +3,6 @@ matplotlib.use('Agg')
 from pyrocko import util
 util.force_dummy_progressbar = True
 util.setup_logging('grondtest', 'info')
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
