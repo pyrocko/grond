44114adeb499dadafff291a8d491515cc08e39cd
mmetz <mmetz@gfz-potsdam.de>
2022-03-16 09:58:33 +0100

    DoublePDR problem

--- src/problems/dynamic_rupture/problem.py
+++ src/problems/dynamic_rupture/problem.py
@@ -252,210 +252,264 @@ class DynamicRuptureProblem(Problem):
         return plots
 
 
-# class LightDynamicRuptureProblemConfig(DynamicRuptureProblemConfig):
-#     base_source_file = Path.T(
-#         optional=True,
-#         help='Yaml file with basesource with e.g. precalculated patches')
+class DoublePDRProblemConfig(ProblemConfig):
 
-#     n_tractions_x = Int.T(default=5)
-#     n_tractions_y = Int.T(default=2)
+    ranges = Dict.T(String.T(), gf.Range.T())
 
-#     slip = Float.T(
-#         optional=True,
-#         help='If given, tractions are scaled to satisfy given slip')
+    decimation_factor = Int.T(
+        default=1,
+        help='Decimation factor of the discretized sub-faults')
+    distance_min = Float.T(default=0.)
+    nthreads = Int.T(default=1)
+    adaptive_resolution = StringChoice.T(
+        choices=('off', 'linear', 'uniqueness'),
+        default='off')
+    adaptive_start = Int.T(
+        default=0)
 
-#     def get_problem(self, event, target_groups, targets):
-#         if self.decimation_factor != 1:
-#             logger.warn(
-#                 'Decimation factor for dynamic rupture source set to %i.'
-#                 ' Results may be inaccurate.' % self.decimation_factor)
+    pure_shear = Bool.T(
+        default=True)
+    smooth_rupture = Bool.T(
+        default=False)
 
-#         # TODO: handle magnitude and slip
-#         if self.base_source_file is None:
-#             base_source = gf.PseudoDynamicRupture.from_pyrocko_event(
-#                 event,
-#                 slip=self.slip,
-#                 anchor=self.anchor,
-#                 decimation_factor=self.decimation_factor,
-#                 nthreads=self.nthreads,
-#                 pure_shear=self.pure_shear,
-#                 smooth_rupture=self.smooth_rupture)
-#         else:
-#             base_source = gf.PseudoDynamicRupture.load(
-#                 filename=self.base_source_file)
-#             base_source.slip = self.slip
+    point_source_target_balancing = Bool.T(
+        default=False,
+        help='If ``True``, target balancing (if used) is performed on a '
+             'moment tensor point source at the events location. It increases '
+             'the speed, but might lead to not fully optimized target weights.'
+        )
 
-#         subs = dict(
-#             event_name=event.name,
-#             event_time=util.time_to_str(event.time))
+    def get_problem(self, event, target_groups, targets):
+        if self.decimation_factor != 1:
+            logger.warn(
+                'Decimation factor for dynamic rupture source set to %i.'
+                ' Results may be inaccurate.' % self.decimation_factor)
 
-#         tractions = LightDynamicRuptureTraction(
-#             length=base_source.length, width=base_source.width,
-#             anchor=base_source.anchor, rake=base_source.rake,
-#             n_tractions_x=self.n_tractions_x, n_tractions_y=self.n_tractions_y)
+        base_source = gf.DoublePDR.from_pyrocko_event(
+            event,
+            nx1=int(self.ranges['nx1'].start),
+            ny1=int(self.ranges['ny1'].start),
+            nx2=int(self.ranges['nx2'].start),
+            ny2=int(self.ranges['ny2'].start),
+            decimation_factor=self.decimation_factor,
+            nthreads=self.nthreads,
+            pure_shear=self.pure_shear,
+            smooth_rupture=self.smooth_rupture,
+            stf=None)
 
-#         base_source.rake = None
+        subs = dict(
+            event_name=event.name,
+            event_time=util.time_to_str(event.time))
 
-#         problem = LightDynamicRuptureProblem(
-#             name=expand_template(self.name_template, subs),
-#             base_source=base_source,
-#             tractions=tractions,
-#             n_tractions_x=self.n_tractions_x,
-#             n_tractions_y=self.n_tractions_y,
-#             magnitude=self.magnitude,
-#             distance_min=self.distance_min,
-#             target_groups=target_groups,
-#             targets=targets,
-#             ranges=self.ranges,
-#             norm_exponent=self.norm_exponent,
-#             nthreads=self.nthreads,
-#             adaptive_resolution=self.adaptive_resolution,
-#             adaptive_start=self.adaptive_start)
+        cmt_problem = None
+        if self.point_source_target_balancing:
+            ranges = dict(
+                time=self.ranges['time'],
+                north_shift=self.ranges['north_shift'],
+                east_shift=self.ranges['east_shift'],
+                depth=self.ranges['depth'],
+                magnitude=gf.Range(
+                    start=event.magnitude - 1.,
+                    stop=event.magnitude + 1.),
+                duration=gf.Range(start=0., stop=event.duration * 2. or 0.),
+                rmnn=gf.Range(start=-1.41421, stop=1.41421),
+                rmee=gf.Range(start=-1.41421, stop=1.41421),
+                rmdd=gf.Range(start=-1.41421, stop=1.41421),
+                rmne=gf.Range(start=-1., stop=1.),
+                rmnd=gf.Range(start=-1., stop=1.),
+                rmed=gf.Range(start=-1., stop=1.))
 
-#         return problem
+            base_source_cmt = gf.MTSource.from_pyrocko_event(event)
 
+            stf = gf.HalfSinusoidSTF()
+            stf.duration = event.duration or 0.0
 
-# class LightDynamicRuptureTraction(TractionField):
-#     n_tractions_x = Int.T(default=5)
-#     n_tractions_y = Int.T(default=2)
+            base_source_cmt.stf = stf
 
-#     length = Float.T(default=10000.)
-#     width = Float.T(default=5000.)
-#     anchor = StringChoice.T(
-#         choices=tuple(map_anchor.keys()),
-#         default='top')
+            cmt_problem = CMTProblem(
+                name=expand_template(self.name_template, subs),
+                base_source=base_source_cmt,
+                distance_min=self.distance_min,
+                target_groups=target_groups,
+                targets=targets,
+                ranges=ranges,
+                mt_type='dc',
+                stf_type='HalfSinusoidSTF',
+                norm_exponent=self.norm_exponent,
+                nthreads=self.nthreads)
 
-#     tractions = Array.T(
-#         optional=True,
-#         dtype=num.float,
-#         shape=(None, None))
+        problem = DoublePDRProblem(
+            name=expand_template(self.name_template, subs),
+            base_source=base_source,
+            distance_min=self.distance_min,
+            target_groups=target_groups,
+            targets=targets,
+            ranges=self.ranges,
+            norm_exponent=self.norm_exponent,
+            nthreads=self.nthreads,
+            adaptive_resolution=self.adaptive_resolution,
+            adaptive_start=self.adaptive_start,
+            cmt_problem=cmt_problem)
 
-#     rake = Float.T(default=0.)
-
-#     def get_tractions(self, nx, ny, patches=None):
-#         if patches is None:
-#             raise ValueError('Patches need to be given')
-
-#         anch_x, anch_y = map_anchor[self.anchor]
-
-#         patch_coords = num.array([
-#             (float(p.ix), float(p.iy))
-#             for p in patches]).reshape(-1, 2)
-
-#         patch_coords[:, 1] -= self.width / 2.
-
-#         pln = self.length / self.n_tractions_x
-#         plw = self.width / self.n_tractions_y
-
-#         coords_x = num.zeros(self.n_tractions_x + 2)
-#         coords_x[1:-1] = [
-#             pln * i + pln/2. for i in range(self.n_tractions_x)]
-#         coords_x[0] = 0
-#         coords_x[-1] = self.length
-#         coords_x -= self.length / 2.
-
-#         coords_y = num.zeros(self.n_tractions_y + 2)
-#         coords_y[1:-1] = [
-#             plw * i + plw/2. for i in range(self.n_tractions_y)]
-#         coords_y[0] = 0
-#         coords_y[-1] = self.width
-#         coords_y -= self.width / 2.
-
-#         tractions = num.zeros((coords_x.shape[0], coords_y.shape[0]))
-#         tractions[1:-1, 1:-1] = self.tractions
-#         tractions[0, 1:-1] = self.tractions[0, :]
-#         tractions[-1, 1:-1] = self.tractions[-1, :]
-#         tractions[1:-1, 0] = self.tractions[:, 0]
-#         tractions[1:-1, -1] = self.tractions[:, -1]
-
-#         tractions[0, 0] = self.tractions[0, 0]
-#         tractions[0, -1] = self.tractions[0, -1]
-#         tractions[-1, 0] = self.tractions[-1, 0]
-#         tractions[-1, -1] = self.tractions[-1, -1]
-
-#         interpolator = RegularGridInterpolator(
-#             (coords_x, coords_y),
-#             tractions)
-
-#         tractions_norm = interpolator(patch_coords)
-
-#         tractions = num.zeros((tractions_norm.shape[0], 3))
-#         tractions[:, 0] = num.cos(self.rake*d2r) * tractions_norm
-#         tractions[:, 1] = num.sin(self.rake*d2r) * tractions_norm
-#         tractions[:, 2] = 0.
-
-#         return tractions
+        return problem
 
 
-# @has_get_plot_classes
-# class LightDynamicRuptureProblem(DynamicRuptureProblem):
+@has_get_plot_classes
+class DoublePDRProblem(Problem):
 
-#     n_tractions_x = Int.T(default=5)
-#     n_tractions_y = Int.T(default=2)
+    problem_parameters = [
+        Parameter('east_shift', 'm', label='Easting', **as_km),
+        Parameter('north_shift', 'm', label='Northing', **as_km),
+        Parameter('depth', 'm', label='Depth', **as_km),
+        Parameter('length1', 'm', label='Length 1', **as_km),
+        Parameter('width1', 'm', label='Width 1', **as_km),
+        Parameter('slip1', 'm', label='Slip 1', optional=True),
+        Parameter('strike1', 'deg', label='Strike 1'),
+        Parameter('dip1', 'deg', label='Dip 1'),
+        Parameter('rake1', 'deg', label='Rake 1'),
+        Parameter('gamma1', 'vr/vs', label=r'$\gamma$ 1'),
+        Parameter('nx1', 'patches', label='nx 1'),
+        Parameter('ny1', 'patches', label='ny 1'),
+        Parameter('length2', 'm', label='Length 2', **as_km),
+        Parameter('width2', 'm', label='Width 2', **as_km),
+        Parameter('slip2', 'm', label='Slip 2', optional=True),
+        Parameter('strike2', 'deg', label='Strike 2'),
+        Parameter('dip2', 'deg', label='Dip 2'),
+        Parameter('rake2', 'deg', label='Rake 2'),
+        Parameter('gamma2', 'vr/vs', label=r'$\gamma$ 2'),
+        Parameter('nx2', 'patches', label='nx 2'),
+        Parameter('ny2', 'patches', label='ny 2'),
+        Parameter('delta_time', 's', label='$\\Delta$ Time'),
+        Parameter('delta_depth', 'm', label='$\\Delta$ Depth'),
+        Parameter('azimuth', 'deg', label='Azimuth'),
+        Parameter('distance', 'm', label='Distance'),
+        Parameter('mix', label='Mix')
+    ]
 
-#     tractions = LightDynamicRuptureTraction.T(optional=True)
+    problem_waveform_parameters = [
+        Parameter('nucleation_x1', 'offset', label='Nucleation X 1'),
+        Parameter('nucleation_y1', 'offset', label='Nucleation Y 1'),
+        Parameter('nucleation_x2', 'offset', label='Nucleation X 2'),
+        Parameter('nucleation_y2', 'offset', label='Nucleation Y 2'),
+        Parameter('time', 's', label='Time'),
+    ]
 
-#     problem_parameters = [
-#         Parameter('t00', 'Pa', label='$t_{00}$'),
-#         Parameter('t01', 'Pa', label='$t_{01}$'),
-#         Parameter('t02', 'Pa', label='$t_{02}$'),
-#         Parameter('t03', 'Pa', label='$t_{03}$'),
-#         Parameter('t04', 'Pa', label='$t_{04}$'),
-#         Parameter('t10', 'Pa', label='$t_{10}$'),
-#         Parameter('t11', 'Pa', label='$t_{11}$'),
-#         Parameter('t12', 'Pa', label='$t_{12}$'),
-#         Parameter('t13', 'Pa', label='$t_{13}$'),
-#         Parameter('t14', 'Pa', label='$t_{14}$')]
+    dependants = []
 
-#     def get_source(self, x):
-#         src = self.base_source
+    distance_min = Float.T(default=0.0)
+    adaptive_resolution = String.T()
+    adaptive_start = Int.T()
 
-#         d = self.get_parameter_dict(x)
-#         p = {k: float(self.ranges[k].make_relative(src[k], d[k]))
-#              for k in src.keys() if k in d}
-#         p['rake'] = None
-#         p['magnitude'] = None
-#         p['slip'] = None
+    cmt_problem = Problem.T(optional=True)
 
-#         tractions = num.zeros((self.n_tractions_y, self.n_tractions_x))
-#         tractions = num.array(
-#             [[float(
-#                 self.ranges['t%i%i' % (i, j)].make_relative(
-#                     0., d['t%i%i' % (i, j)]))
-#                 for i in range(self.n_tractions_y)]
-#                 for j in range(self.n_tractions_x)])
+    def set_engine(self, engine):
+        self._engine = engine
 
-#         self.tractions.tractions = tractions
+        if self.cmt_problem is not None:
+            self.cmt_problem.set_engine(engine)
 
-#         return src.clone(tractions=self.tractions, **p)
+    def pack(self, source):
+        arr = self.get_parameter_array(source)
+        for ip, p in enumerate(self.parameters):
+            if p.name == 'time':
+                arr[ip] -= self.base_source.time
+        return arr
 
-#     def random_uniform(self, xbounds, rstate, fixed_magnitude=None):
-#         if fixed_magnitude is not None:
-#             raise GrondError(
-#                 'Setting fixed magnitude in random model generation not '
-#                 'supported for this type of problem.')
+    def get_source(self, x):
+        src = self.base_source
 
-#         x = num.zeros(self.nparameters)
-#         for i in range(self.nparameters):
-#             x[i] = rstate.uniform(xbounds[i, 0], xbounds[i, 1])
+        d = self.get_parameter_dict(x)
+        p = {k: float(self.ranges[k].make_relative(src[k], d[k]))
+             for k in src.keys() if k in d}
 
-#         return x
+        p['nx1'] = int(p['nx1'])
+        p['ny1'] = int(p['ny1'])
 
-#     def preconstrain(self, x, optimiser=None):
-#         return x
+        if p['nx1'] != src.nx1 or p['ny1'] != src.ny1:
+            logger.info('refining patches to %dx%d', p['nx1'], p['ny1'])
 
-#     @classmethod
-#     def get_plot_classes(cls):
-#         from . import plot
-#         plots = super(LightDynamicRuptureProblem, cls).get_plot_classes()
-#         plots.extend([
-#             plot.DynamicRuptureSlipMap,
-#             plot.DynamicRuptureSTF,
-#             plot.DynamicRuptureMap])
-#         return plots
+        src.nx1 = p['nx1']
+        src.ny1 = p['ny1']
+
+        p['nx2'] = int(p['nx2'])
+        p['ny2'] = int(p['ny2'])
+
+        if p['nx2'] != src.nx2 or p['ny2'] != src.ny2:
+            logger.info('refining patches to %dx%d', p['nx2'], p['ny2'])
+
+        src.nx2 = p['nx2']
+        src.ny2 = p['ny2']
+
+        return src.clone(**p)
+
+    def random_uniform(self, xbounds, rstate, fixed_magnitude=None):
+        if fixed_magnitude is not None:
+            raise GrondError(
+                'Setting fixed magnitude in random model generation not '
+                'supported for this type of problem.')
+
+        x = num.zeros(self.nparameters)
+        for i in range(self.nparameters):
+            x[i] = rstate.uniform(xbounds[i, 0], xbounds[i, 1])
+
+        return x
+
+    def preconstrain(self, x, optimiser=None):
+        nx1 = self.ranges['nx1']
+        ny1 = self.ranges['ny1']
+        nx2 = self.ranges['nx2']
+        ny2 = self.ranges['ny2']
+
+        if optimiser and self.adaptive_resolution == 'linear' and \
+                optimiser.iiter >= self.adaptive_start:
+
+            progress = (optimiser.iiter - self.adaptive_start - 1) / \
+                (optimiser.niterations - self.adaptive_start)
+
+            nx1 = num.floor(
+                nx1.start + progress * (nx1.stop - nx1.start + 1))
+            ny1 = num.floor(
+                ny1.start + progress * (ny1.stop - ny1.start + 1))
+            nx2 = num.floor(
+                nx2.start + progress * (nx2.stop - nx2.start + 1))
+            ny2 = num.floor(
+                ny2.start + progress * (ny2.stop - ny2.start + 1))
+
+        elif optimiser and self.adaptive_resolution == 'uniqueness' and \
+                optimiser.iiter >= self.adaptive_start:
+            raise NotImplementedError
+
+        else:
+            nx1 = nx1.start
+            ny1 = ny1.start
+            nx2 = nx2.start
+            ny2 = ny2.start
+
+        idx_nx1 = self.get_parameter_index('nx1')
+        idx_ny1 = self.get_parameter_index('ny1')
+        idx_nx2 = self.get_parameter_index('nx2')
+        idx_ny2 = self.get_parameter_index('ny2')
+        x[idx_nx1] = int(round(nx1))
+        x[idx_ny1] = int(round(ny1))
+        x[idx_nx2] = int(round(nx2))
+        x[idx_ny2] = int(round(ny2))
+
+        return x
+
+    @classmethod
+    def get_plot_classes(cls):
+        from . import plot
+        plots = super(DoublePDRProblem, cls).get_plot_classes()
+        # plots.extend([
+        #     plot.DynamicRuptureSlipMap,
+        #     plot.DynamicRuptureSTF,
+        #     plot.DynamicRuptureMap])
+        return plots
 
 
 __all__ = '''
     DynamicRuptureProblem
     DynamicRuptureProblemConfig
+    DoublePDRProblem
+    DoublePDRProblemConfig
 '''.split()
